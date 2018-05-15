47685ab543b3d8e737c2791bd9bd923e8daf1c13
Nicholas Edwards <edwardsnjd@googlemail.com>
2018-05-15 18:38:54 +0100

    Add support for state diagrams

--- README.md
+++ README.md
@@ -3,7 +3,7 @@ Diagram builder
 
 Simple utility to help building diagrams from text files.
 
-Currently only supports sequence diagrams.
+Currently supports sequence and state diagrams.
 
 Usage
 -----
@@ -13,3 +13,7 @@ Usage
 Where:
 - `inputDir` - the path to a directory of input docs
 - `outputDir` - the path to a directory to contain output diagrams
+
+File extensions are used to determine which type of diagram to build:
+- `*.wsd` - a sequence diagram
+- `*.state` - a state diagram
--- main.js
+++ main.js
@@ -1,21 +1,55 @@
 const FileStorage = require('./lib/FileStorage');
 const getSequenceDiagram = require('./lib/getSequenceDiagram');
+const getStateDiagram = require('./lib/getStateDiagram');
+
+const DIAGRAM_TYPES = {
+  sequence: 'sequence',
+  state: 'state',
+  unknown: 'unknown',
+};
+
+function findDiagramTypeByExtension(relativePath, storage) {
+  const extension = storage.getExtension(relativePath);
+  const extensions = {
+    [DIAGRAM_TYPES.sequence]: '.wsd',
+    [DIAGRAM_TYPES.state]: '.state',
+  };
+  const diagramType = Object.keys(extensions).find(type => extensions[type] === extension);
+  return diagramType || DIAGRAM_TYPES.unknown;
+}
+
+function findGenerator(diagramType) {
+  const generators = {
+    [DIAGRAM_TYPES.sequence]: (diagramText) => getSequenceDiagram(diagramText, {format: 'png', style: 'modern-blue'}),
+    [DIAGRAM_TYPES.state]:    (diagramText) => getStateDiagram(diagramText, {format: 'png'}),
+  };
+  return generators[diagramType];
+}
+
+function handleDiagram(relativePath, storage) {
+  console.log('Processing file', relativePath);
+  const diagramType = findDiagramTypeByExtension(relativePath, storage);
+  const generator = findGenerator(diagramType);
+
+  if (!generator) {
+    return Promise.reject(`No diagram generator found for file '${relativePath}' (type: '${diagramType}')`);
+  }
+
+  const diagramText = storage.readInputFile(relativePath);
+  return generator(diagramText)
+    .then(({data}) => storage.writeOutputFile(`${relativePath}.png`, data));
+}
 
 function main(inputDir, outputDir) {
   const storage = new FileStorage({ inputDir, outputDir });
+
   return storage.getInputFiles()
     .reduce(
-      (p, relativePath) => p.then(() => handleSequenceDiagram(relativePath, storage, {style: 'modern-blue'})),
+      (p, relativePath) =>
+        p.catch(console.error)
+          .then(() => handleDiagram(relativePath, storage)),
       Promise.resolve()
     );
-}
-
-function handleSequenceDiagram(relativePath, storage, opts={}) {
-  console.log('Processing file', relativePath);
-  const diagramText = storage.readInputFile(relativePath);
-
-  return getSequenceDiagram(diagramText, opts)
-    .then(({data}) => storage.writeOutputFile(`${relativePath}.png`, data));
 }
 
 module.exports = main;
